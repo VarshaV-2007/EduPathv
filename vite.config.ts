0d8f81d940f2d12ab581f18aac2771638200eb8e
Varsha <varshavelmurugan28@gmail.com>
2026-09-21 23:57:20 +0530

    Configure Nitro production server

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: { port: 8080 },
   resolve: { tsconfigPaths: true },
-  plugins: [tanstackStart(), viteReact(), tailwindcss()],
+  plugins: [tanstackStart(),nitro(), viteReact(), tailwindcss()],
 });
